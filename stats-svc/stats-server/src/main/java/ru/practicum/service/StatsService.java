3127727d7206e610871bf4f3bca3198ebb0eef1d
gray-alt <gray-alt@yandex.ru>
2023-08-25 20:07:54 +0300

    style: добавить перенос строки

--- stats-svc/stats-server/src/main/java/ru/practicum/service/StatsService.java
+++ stats-svc/stats-server/src/main/java/ru/practicum/service/StatsService.java
@@ -8,5 +8,6 @@ import java.util.Collection;
 
 public interface StatsService {
     void saveHit(Hit hit);
+
     Collection<Stats> getStats(LocalDateTime start, LocalDateTime end, Collection<String> uris, boolean unique);
 }
